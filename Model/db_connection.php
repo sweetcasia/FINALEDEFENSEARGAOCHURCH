4dca206b43e9d215c652dccc6523556db9cdd888
sweetcasia <sweetvenicecasia10@gmail.com>
2024-11-14 11:19:41 +0800

    update

--- Model/db_connection.php
+++ Model/db_connection.php
@@ -1,12 +1,19 @@
 <?php
-$servername = "localhost"; 
-$username = "root"; 
-$password = ""; 
-$dbname = "churchargao"; 
+// Fetch database configuration from environment variables
+$server = getenv('DB_HOST');          // Fetches DB_HOST from environment variables
+$dbname = getenv('DB_DATABASE');      // Fetches DB_DATABASE from environment variables
+$dbuser = getenv('DB_USERNAME');      // Fetches DB_USERNAME from environment variables
+$dbpass = getenv('DB_PASSWORD');      // Fetches DB_PASSWORD from environment variables
+$dbport = getenv('DB_PORT') ?: '3306'; // Fetches DB_PORT from environment variables or defaults to 3306
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Create a new mysqli instance with the provided credentials
+$conn = new mysqli($server, $dbuser, $dbpass, $dbname, $dbport);
 
+// Check the connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+// Your code here
+
 ?>
